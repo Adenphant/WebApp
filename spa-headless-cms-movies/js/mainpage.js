c2e2e5a012f1cbb359363be403ec8f72f2a37b9f
DanielTjerrildGamborg <eaadaga@students.eaaa.dk>
2021-09-29 21:18:55 +0200

    genres on genre page

--- spa-headless-cms-movies/js/mainpage.js
+++ spa-headless-cms-movies/js/mainpage.js
@@ -2,19 +2,19 @@
 
 
 
-let _hiphopContent = [];
+let _content = [];
 
 // fetch featured genres (hip hop)
-async function getHiphop() {
-  let response = await fetch("http://wordpress.oscho.dk//wp-json/wp/v2/posts?_embed&categories=5");
+async function getGenres() {
+  let response = await fetch("http://wordpress.oscho.dk//wp-json/wp/v2/posts?_embed&categories=19");
   let data = await response.json();
   console.log(data);
-  _hiphopContent = data;
-  appendHiphop(data);
+  _content = data;
+  appendGenres(data);
   //showLoader(false);
 }
 
-getHiphop();
+getGenres();
 
 //fetch featured artists
 
@@ -22,7 +22,7 @@ async function getArtists() {
   let response = await fetch("http://wordpress.oscho.dk//wp-json/wp/v2/posts?_embed&categories=18");
   let data = await response.json();
   console.log(data);
-  _hiphopContent = data;
+  _content = data;
   appendArtists(data);
   //showLoader(false);
 }
@@ -30,7 +30,7 @@ async function getArtists() {
 getArtists();
 
 // append hiphop to the DOM
-function appendHiphop(contents) {
+function appendGenres(contents) {
   let htmlTemplate = "";
   for (let content of contents) {
     htmlTemplate += /*html*/ `
@@ -40,7 +40,7 @@ function appendHiphop(contents) {
       </article>
     `;
   }
-  document.querySelector('#hiphop-container').innerHTML += htmlTemplate;
+  document.querySelector('#featured-container').innerHTML += htmlTemplate;
 }
 
 // append content to the DOM
@@ -56,6 +56,171 @@ function appendArtists(contents) {
   }
   document.querySelector('#artist-container').innerHTML += htmlTemplate;
 }
+
+//fetch right now
+
+async function getRightNow() {
+  let response = await fetch("http://wordpress.oscho.dk//wp-json/wp/v2/posts?_embed&categories=20");
+  let data = await response.json();
+  console.log(data);
+  _content = data;
+  appendRightNow(data);
+  //showLoader(false);
+}
+
+getRightNow();
+
+// append genres to the DOM
+function appendRightNow(contents) {
+  let htmlTemplate = "";
+  for (let content of contents) {
+    htmlTemplate += /*html*/ `
+      <article>
+      <img src="${getFeaturedImageUrl(content)}">
+              
+      </article>
+    `;
+  }
+  document.querySelector('#rightnow-container').innerHTML += htmlTemplate;
+}
+
+//genres page
+//fetch hiphop genre
+
+async function getHipHop() {
+  let response = await fetch("http://wordpress.oscho.dk//wp-json/wp/v2/posts?_embed&categories=5");
+  let data = await response.json();
+  console.log(data);
+  _content = data;
+  appendHipHop(data);
+  //showLoader(false);
+}
+
+getHipHop();
+
+// append hiphop to the DOM
+function appendHipHop(contents) {
+  let htmlTemplate = "";
+  for (let content of contents) {
+    htmlTemplate += /*html*/ `
+      <article>
+      <img src="${getFeaturedImageUrl(content)}">
+              
+      </article>
+    `;
+  }
+  document.querySelector('#hiphop-container').innerHTML += htmlTemplate;
+}
+
+//fetch pop genre
+
+async function getPop() {
+  let response = await fetch("http://wordpress.oscho.dk//wp-json/wp/v2/posts?_embed&categories=17");
+  let data = await response.json();
+  console.log(data);
+  _content = data;
+  appendPop(data);
+  //showLoader(false);
+}
+
+getPop();
+
+// append hiphop to the DOM
+function appendPop(contents) {
+  let htmlTemplate = "";
+  for (let content of contents) {
+    htmlTemplate += /*html*/ `
+      <article>
+      <img src="${getFeaturedImageUrl(content)}">
+              
+      </article>
+    `;
+  }
+  document.querySelector('#pop-container').innerHTML += htmlTemplate;
+}
+
+//fetch rock genre
+
+async function getRock() {
+  let response = await fetch("http://wordpress.oscho.dk//wp-json/wp/v2/posts?_embed&categories=6");
+  let data = await response.json();
+  console.log(data);
+  _content = data;
+  appendRock(data);
+  //showLoader(false);
+}
+
+getRock();
+
+// append hiphop to the DOM
+function appendRock(contents) {
+  let htmlTemplate = "";
+  for (let content of contents) {
+    htmlTemplate += /*html*/ `
+      <article>
+      <img src="${getFeaturedImageUrl(content)}">
+              
+      </article>
+    `;
+  }
+  document.querySelector('#rock-container').innerHTML += htmlTemplate;
+}
+
+//fetch metal genre
+
+async function getMetal() {
+  let response = await fetch("http://wordpress.oscho.dk//wp-json/wp/v2/posts?_embed&categories=14");
+  let data = await response.json();
+  console.log(data);
+  _content = data;
+  appendMetal(data);
+  //showLoader(false);
+}
+
+getMetal();
+
+// append hiphop to the DOM
+function appendMetal(contents) {
+  let htmlTemplate = "";
+  for (let content of contents) {
+    htmlTemplate += /*html*/ `
+      <article>
+      <img src="${getFeaturedImageUrl(content)}">
+              
+      </article>
+    `;
+  }
+  document.querySelector('#metal-container').innerHTML += htmlTemplate;
+}
+
+//fetch R&b genre
+
+async function getRnb() {
+  let response = await fetch("http://wordpress.oscho.dk//wp-json/wp/v2/posts?_embed&categories=13");
+  let data = await response.json();
+  console.log(data);
+  _content = data;
+  appendRnb(data);
+  //showLoader(false);
+}
+
+getRnb();
+
+// append hiphop to the DOM
+function appendRnb(contents) {
+  let htmlTemplate = "";
+  for (let content of contents) {
+    htmlTemplate += /*html*/ `
+      <article>
+      <img src="${getFeaturedImageUrl(content)}">
+              
+      </article>
+    `;
+  }
+  document.querySelector('#rnb-container').innerHTML += htmlTemplate;
+}
+
+
 
 function getFeaturedImageUrl(post) {
   let imageUrl = "";
